6eae5d8cec62215465a4e5dcc8dc05e2d4ec141e
mihailermak <122021329+mihailermak@users.noreply.github.com>
2023-03-16 21:13:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ TOKEN = '5894098532:AAGOr_Hj2VSAbCRtrFON6Bg36kuexi9tqXo'
 
 bot = telebot.TeleBot(TOKEN)
 
-words = ['python', 'bot', 'hungmangame']
+words = ['лето', 'солнце', 'тепло']
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -15,23 +15,28 @@ def start_message(message):
 def play_game(message):
     word = random.choice(words)
     hidden_word = list('_' * len(word))
-    bot.reply_to(message, f"Я загадал слово из {len(word)} букв. Это слово: {' '.join(hidden_word)}, а ответ: {word}. Введите букву:")
+    bot.reply_to(message, f"Я загадал слово из {len(word)} букв: {' '.join(hidden_word)}. Введите букву:")
 
     @bot.message_handler(func=lambda message: message.text.isalpha())
     def guess_letter(message):
-        nonlocal word
         nonlocal hidden_word
-        if message.text in word:
-            for i in range(len(word)):
-                if word[i] == message.text:
-                    hidden_word[i] = message.text
 
         if message.text in hidden_word:
             bot.reply_to(message, f"Вы уже угадали букву {message.text}. Введите другую:")
             return
 
-        if '_' not in hidden_word:
-            bot.reply_to(message, f"Вы выиграли! Загаданное слово было '{word}'.")
-            return bot.reply_to(message, f"Вы угадали букву {message.text}. Текущее состояние слова: {' '.join(hidden_word)}")
+        if message.text in word:
+            for i in range(len(word)):
+                if word[i] == message.text:
+                    hidden_word[i] = message.text
+
+            if '_' not in hidden_word:
+                bot.reply_to(message, f"Вы выиграли! Загаданное слово было '{word}'. Чтобы начать игру заново, напишите /play")
+                return
+
+            bot.reply_to(message, f"Вы угадали букву {message.text}. Текущее состояние слова: {' '.join(hidden_word)}. Введите следующую букву:")
+
+        else:
+            bot.reply_to(message, f"Буква {message.text} не входит в загаданное слово. Введите следующую букву:")
 
 bot.polling(none_stop=True)
